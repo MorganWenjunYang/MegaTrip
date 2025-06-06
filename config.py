a27112022ace0cab8e5086ee40925a2b636bab4e
Morgan Wenjun Yang <wy2347@columbia.edu>
2025-06-06 13:39:23 +0800

    fix env error

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ DB_CONFIG = {
     'host': os.getenv('MYSQL_HOST', 'localhost'),
     'user': os.getenv('MYSQL_USER', 'your_username'),
     'password': os.getenv('MYSQL_PASSWORD', 'your_password'),
-    'database': os.getenv('MYSQL_DB', 'your_database'),
+    'database': os.getenv('MYSQL_DATABASE', 'your_database'),
     'port': int(os.getenv('MYSQL_PORT', 3306))
 }
